7bdb1eb115366f8090d153bfa043c0d7766e70e6
Ondřej Kupka <ondra.cap@gmail.com>
2016-05-30 13:43:18 +0200

    wip [ci skip]

--- context.go
+++ context.go
@@ -69,14 +69,40 @@ func (ctx *Context) blockFetcher() error {
 
 		select {
 		case ctx.blockCh <- block:
-		case ctx.t.Dying:
+		case ctx.t.Dying():
 			return nil
 		}
 	}
 }
 
 func (ctx *Context) mapper() error {
+	select {
+	case block := <-ctx.blockCh:
+		if err := mapreduce.Map(ctx.client, block); err != nil {
+			return err
+		}
+	case <-ctx.t.Dying():
+		return nil
+	}
+}
 
+func (ctx *Context) reducer() error {
+	fmt.Println("---> Reducer: Getting the initial value ...")
+	acc, err := mapreduce.InitialValue(ctx.client)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("---> Reducer: Starting to process incoming blocks ...")
+	select {
+	case next := <-ctx.blockCh:
+		acc, err = mapreduce.Reduce(acc, next)
+		if err != nil {
+			return err
+		}
+	case ctx.t.Dying():
+		return nil
+	}
 }
 
 func (ctx *Context) emit(value interface{}) error {
